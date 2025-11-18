805ad8580ea771e1db2191948e7d0d0b11ccc867
Vdujour <valentin.dujour@etu.univ-poitiers.fr>
2025-11-18 18:33:43 +0100

    feat: ✨ import good things to create dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,12 +1,39 @@
+import { useState, useEffect } from 'react';
+import { buildTitle } from '../../constants/appMeta.js';
+import { useRequireToken } from '../../hooks/useRequireToken.js';
+import { useNavigate } from 'react-router-dom';
 import './DashboardPage.css';
 import '../PageLayout.css';
 
-const DashboardPage = () => {
+import TrackItem from '../../components/TrackItem/TrackItem.jsx';
+import TopArtistItem from '../../components/TopArtistItem/TopArtistItem.jsx';
+import { fetchUserTopTracks } from '../../api/spotify-me.js';
+import { fetchUserTopArtists } from '../../api/spotify-me.js';
+import { fetchUserPlaylists } from '../../api/spotify-me.js';
+
+export default function DashboardPage() {
+    // Initialize navigate function
+    const navigate = useNavigate();
+
+    // state for playlists data
+    const [playlists, setPlaylists] = useState([]);
+
+    // state for loading and error
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // require token to fetch playlists
+    const { token } = useRequireToken();
+
+    // Set document title
+    useEffect(() => { document.title = buildTitle('Playlists'); }, []);
+
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Dashboard Page</h1>
         </div>
     );
-};
 
-export default DashboardPage;
+}
+
