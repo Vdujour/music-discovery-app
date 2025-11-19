5a4810e8a6674dbd1f91036c930caac3e1b9e810
Vdujour <valentin.dujour@etu.univ-poitiers.fr>
2025-11-19 17:29:30 +0100

    feat: ✨ ask to api Playlist and Artist

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -7,12 +7,19 @@ import '../PageLayout.css';
 
 import { fetchUserTopTracks, fetchUserTopArtists } from '../../api/spotify-me.js';
 
+const limit = 1;
+
+const timeRange = 'short_term';
+
 export default function DashboardPage() {
     // Initialize navigate function
     const navigate = useNavigate();
 
-    // state for playlists data
-    const [playlists, setPlaylists] = useState([]);
+    // state for artists data
+    const [artists, setArtists] = useState([]);
+
+    // state for tracks data
+    const [tracks, setTracks] = useState([]);
 
     // state for loading and error
     const [loading, setLoading] = useState(true);
@@ -24,10 +31,40 @@ export default function DashboardPage() {
     // Set document title
     useEffect(() => { document.title = buildTitle('Dashboard'); }, []);
 
+    useEffect(() => {
+        if (!token) return; // wait for auth check
+        // fetch user top artists when token changes
+        fetchUserTopArtists(token, limit, timeRange)
+          .then(res => {
+            if (res.error) {
+              if (!handleTokenError(res.error, navigate)) {
+                setError(res.error);
+              }
+            }
+            setArtists(res.data.items);
+          })
+          .catch(err => { setError(err.message); })
+          .finally(() => { setLoading(false); });
+
+        // fetch user top tracks when token changes
+        fetchUserTopTracks(token, limit, timeRange)
+            .then(res => {
+            if (res.error) {
+                if (!handleTokenError(res.error, navigate)) {
+                setError(res.error);
+                }
+            }
+            setTracks(res.data.items);
+            })
+            .catch(err => { setError(err.message); })
+            .finally(() => { setLoading(false); });
+      }, [token, navigate]);
 
     return (
         <div>
             <h1>Dashboard Page</h1>
+            <p>Top Artists: {artists.length}</p>
+            <p>Top Tracks: {tracks.length}</p>
         </div>
     );
 
